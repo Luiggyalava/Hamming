6593abfdaf58e6a001365180978778554c02d2ca
Luiggy Alava <steven.alavavivanco@tiernoparla.es>
2022-11-28 10:44:24 +0100

    calcular tamaño del array

--- Ejercicio_Hamming_Steven_Alava/src/main/java/tiernoparla/ejercicio_hamming_steven_alava/Hamming.java
+++ Ejercicio_Hamming_Steven_Alava/src/main/java/tiernoparla/ejercicio_hamming_steven_alava/Hamming.java
@@ -7,7 +7,8 @@ public class Hamming {
     public static void main(String[] args) {
          //SENDER
         int msg[] = {1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1};
-       
+        //calcular el total del array
+        final int longmsg = msg.length;
     }//cierre main
 
 }//cierre Clase
