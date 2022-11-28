77e2d9d1eef5279225e4c78a06c0ffc757fcdc1a
Luiggy Alava <steven.alavavivanco@tiernoparla.es>
2022-11-28 10:46:47 +0100

    calculo multiplo de 2 para bit de pariedad

--- Ejercicio_Hamming_Steven_Alava/src/main/java/tiernoparla/ejercicio_hamming_steven_alava/Hamming.java
+++ Ejercicio_Hamming_Steven_Alava/src/main/java/tiernoparla/ejercicio_hamming_steven_alava/Hamming.java
@@ -2,13 +2,23 @@ package tiernoparla.ejercicio_hamming_steven_alava;
 
 //import java.util.Random;
 //import java.util.Scanner;
-
 public class Hamming {
+
     public static void main(String[] args) {
-         //SENDER
+        //SENDER
         int msg[] = {1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1};
+
         //calcular el total del array
         final int longmsg = msg.length;
+
+        //Calcular la Potencia de dos necesarias para el bit de pariedad
+        final int potencia = 2;
+        int bitpar = 0;
+  
+        while (Math.pow(potencia, bitpar) < bitpar + longmsg + 1) {
+            bitpar++;
+        }//while
+        
     }//cierre main
 
 }//cierre Clase
