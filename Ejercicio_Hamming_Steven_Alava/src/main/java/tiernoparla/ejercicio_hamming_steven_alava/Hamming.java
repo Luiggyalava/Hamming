e1040670178c33232eeed2cea9fefed7a6c63497
Luiggy Alava <steven.alavavivanco@tiernoparla.es>
2022-11-28 10:41:15 +0100

    creacion de array fijo

---
+++ Ejercicio_Hamming_Steven_Alava/src/main/java/tiernoparla/ejercicio_hamming_steven_alava/Hamming.java
@@ -0,0 +1,14 @@
+package tiernoparla.ejercicio_hamming_steven_alava;
+
+//import java.util.Random;
+//import java.util.Scanner;
+
+public class Hamming {
+    public static void main(String[] args) {
+         //SENDER
+        int msg[] = {1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1};
+       
+    }//cierre main
+
+}//cierre Clase
+
