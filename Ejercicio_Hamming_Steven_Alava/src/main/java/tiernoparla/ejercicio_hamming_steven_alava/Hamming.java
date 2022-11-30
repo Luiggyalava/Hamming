055dc2e5ff6ba8ee001365974b78032b609d3a44
Luiggy Alava <steven.alavavivanco@tiernoparla.es>
2022-11-30 09:35:19 +0100

    codigo correcto sin averiguar posiciones de fallo

--- Ejercicio_Hamming_Steven_Alava/src/main/java/tiernoparla/ejercicio_hamming_steven_alava/Hamming.java
+++ Ejercicio_Hamming_Steven_Alava/src/main/java/tiernoparla/ejercicio_hamming_steven_alava/Hamming.java
@@ -1,23 +1,21 @@
 package tiernoparla.ejercicio_hamming_steven_alava;
 
-//import java.util.Random;
-
 import java.util.Random;
 import java.util.Scanner;
 
-//import java.util.Scanner;
 public class Hamming {
 
     public static void main(String[] args) {
-
         //SENDER
         //Longitud y Creacion del mensaje aleatorio
         Scanner longitud = new Scanner(System.in);
+        Random rnd = new Random();
+        
         int longmsg;
         System.out.println("Indicame longitud del mensaje aleatorio");
         longmsg = longitud.nextInt();
         int[] msg = new int[longmsg];
-        Random rnd = new Random();
+        
 
         System.out.println("Tu mensaje es: ");
         for (int i = 0; i < msg.length; i++) {
@@ -27,100 +25,90 @@ public class Hamming {
         System.out.println(" ");
 
         //Calcular la Potencia de dos necesarias para el bit de pariedad
-        final int potencia = 2;
+        final int POTENCIA = 2;
         int bitpar = 0;
-
-        while (Math.pow(potencia, bitpar) < bitpar + longmsg + 1) {
+        while (Math.pow(POTENCIA, bitpar) < bitpar + longmsg + 1) {
             bitpar++;
         }//while  
 
         System.out.println("Los bits de pariedad que usaremos son: " + bitpar);
-
-        //**********************************************************************
+        //***********************************************************
         //añadimos el nuevo array con la longitud + pariedad + 1
-        final int tam = longmsg + (bitpar + 1);
+        final int TAM = longmsg + (bitpar + 1);
+        int[] newmsg = new int[TAM];
+        System.out.println("El tamaño del array nuevo + bitpar es de :" + TAM);
 
-        System.out.println("El tamaño del array nuevo + bitpar es de :" + tam);
-        int[] newmsg = new int[tam];
-
-        //variable exponente para reutilizar*
+        //variables de uso
         int exponente = 0;
-
-        /*bucle para agregar el mensaje al array nuevo e identificar posicion
-        del bit de pariedad */
         int contmsg = 0;
 
         //calcular posicion de Bit de Pariedad
-        calcularPosBit(msg, newmsg, potencia, exponente, contmsg);
+        calcularPosBit(msg, newmsg, POTENCIA, exponente, contmsg);
         System.out.println(" ");
 
         //Contar 0's y 1's del mensaje para calcular modificar el bit de pariedad
         System.out.println("El mensaje calculando los bits de pariedad es: ");
-
         contarDependientesBitPar(newmsg);
         System.out.println(" ");
 
         //Contar todas las posiciones y colocar Bit global
         calcularBitGlobal(newmsg);
-
         System.out.println("El Bit global es: " + newmsg[0]);
         //Test OK
-        
-//*********************************************************************          
+        //***********************************************************          
         //NOISE
         //Creamos un nuevo array con el mensaje de ruido para tratarlo
         System.out.println("El mensaje original es: ");
-        int[] noisemsg = new int[tam];
+        int[] noisemsg = new int[TAM];
         arraysNuevos(noisemsg, newmsg);
         System.out.println(" ");
 
         //Introducimos un mensaje aleatorio dentro del array nuevo
-        int contf = 0;
-        int aleatorio = (int) (Math.random() * tam + 1);
         System.out.println("El mensaje con ruido es: ");
-        agregarFallos(noisemsg, aleatorio, contf, tam);
+        agregarFallos(noisemsg, TAM);
         System.out.println(" ");
         //Test OK
-//*********************************************************************
+//*******************************************************************
         //RECIEVER
         //Crear nuevo array de respuesta para tratarlo
         System.out.println("El mensaje recibido es: ");
-        int[] recevermsg = new int[tam];
+        int[] recevermsg = new int[TAM];
         arraysNuevos(recevermsg, noisemsg); //cierre for
         System.out.println(" ");
 
         //Contar 0's y 1's del mensaje para calcular modificar el bit de pariedad
         System.out.println("El mensaje calculando los bits de pariedad es: ");
-
         contarDependientesBitPar(recevermsg);
         System.out.println(" ");
 
         //Contar todas las posiciones y colocar Bit global
         calcularBitGlobal(recevermsg);
         System.out.println("El Bit global es: " + recevermsg[0]);
-    }//main
 
-    public static void agregarFallos(int[] noisemsg, int aleatorio, int contf,
-                                     final int tam) {
+    }//main
+    public static void agregarFallos(int[] noisemsg, final int TAM) {
+        int contf = 0;
+        int aleatorio = 0;
         for (int i = 0; i < noisemsg.length; i++) {
-            if (aleatorio <= i && contf != 2) {
+            aleatorio = (int) (Math.random() * TAM + 1);
+            /*se pone (TAM/3) para que las posibilidades sean 1/3 de tener
+            2 fallos */
+            if (aleatorio <= (TAM / 3) && contf != 2) {
                 noisemsg[i] = (int) (Math.random() * 1 + 1);
                 contf++;
-            } else {
-                aleatorio = (int) (Math.random() * tam + 1);
-            }//else
+            }
             System.out.print(noisemsg[i]);
-        }//cierre for
+        }
     }
 
-    private static void arraysNuevos(int[] array1, int[] array2) {
+    public static void arraysNuevos(int[] array1, int[] array2) {
         for (int i = 0; i < array1.length; i++) {
             array1[i] = array2[i];
             System.out.print(array1[i]);
         }
     }
 
-    private static void calcularBitGlobal(int[] newmsg) {
+    public static void calcularBitGlobal(int[] newmsg) {
         int cont1 = 0;
         for (int i = 1; i < newmsg.length; i++) {
             if (newmsg[i] == 1) {
@@ -129,13 +117,13 @@ public class Hamming {
         }
         if ((cont1 % 2) == 0) {
             newmsg[0] = 0;
-        }//cierre if
+        }
         else {
             newmsg[0] = 1;
         }
     }
 
-    private static void contarDependientesBitPar(int[] newarr) {
+    public static void contarDependientesBitPar(int[] newarr) {
         int cont1 = 0;
         for (int x = 1; x < newarr.length; x++) {
             for (int z = 1; z < newarr.length; z++) {
@@ -155,14 +143,12 @@ public class Hamming {
         }
     }
 
-    private static void calcularPosBit(int[] msg, int[] newarr, final int
-                                       potencia, int exponente, int contmsg) {
+    public static void calcularPosBit(int[] msg, int[] newarr, final int POTENCIA, int exponente, int contmsg) {
         for (int i = 1; i < newarr.length; i++) {
-            if (Math.pow(potencia, exponente) != i) {
+            if (Math.pow(POTENCIA, exponente) != i) {
                 newarr[i] = msg[contmsg];
                 contmsg++;
-            }
-            else {
+            } else {
                 newarr[i] = 2;
                 exponente++;
             }
@@ -170,4 +156,3 @@ public class Hamming {
         }
     }
 }//cierre Clase
-
